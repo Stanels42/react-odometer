8b6227c56c91f48dd0084dacb6fc8c0a2e908822
Chris Stanley <CStanley4848@gmail.com>
2020-01-28 21:16:14 -0800

    Refactored the Odometer out into it's own file, Updated Read Me

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-odometer
-Lab 37 create an odometer with react.js
+Lab 37 create an odometer with react.js<br>
+[Odometer Code](./odometer/src/Odometer.js)
+
+## Assignment
+The goal of this lab is to create a 4 digit Odometer using the React framework. The application should have buttons that allow you to reset the value. Once the value hits a certain point it should either roll over or cut off.
+
+## Approach
+All the real logic is in the Odometer. The main parts I would like to point out are the button display and event handler. For the buttons I used a function component that constructs a set of increase and decrease buttons. This function does take in a value that represents the number of '0's after a one, this is used in both the name as well as an ID. As for the event handler it uses the ID of the button clicked to change the Odometer by that amount. Before the value is saved it is checked if the value is out of bounds and resets it to be in the valid range.
--- odometer/src/App.js
+++ odometer/src/App.js
@@ -3,71 +3,18 @@ import './App.css';
 import HeaderComponent from './header';
 import FooterComponent from './footer';
 
+import Odometer from './Odometer'
+
 class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      odometer :0,
-      maxDisplay: 10000,
-    };
-    this.increment = this.increment.bind(this)
-  }
-  
-  /**
-   * ## Event Listener ##
-   * Used to adjust the odometer based on the value of the button pushed.
-   */
-  increment(event) {
-    let change = parseInt(event.target.id);
-    change = (this.state.odometer + change)
-    change = change < 0 ? 0 : change%this.state.maxDisplay
-    this.setState({
-      odometer: change,
-    })
-  }
-
-  /**
-   * Takes in the current odometer value and returns the displayable version.
-   * In: Int
-   * Out: String
-   */
-  displayNumber(number) {
-    let odo = String(number);
-    return '0'.repeat(4-odo.length > 0? 4-odo.length : 0) + odo
-  }
-
-  /**
-   * A component function used to create the buttons. Takes in a value that is the number of '0's after the 1.
-   * In: Int
-   * Out: An increase and decrease button
-   */
-  createButtonSet (val) {
-    let idNum = "1" + '0'.repeat(val)
-    return(
-    <li key={val}>
-      <button onClick={this.increment} id={idNum}>Add {idNum}</button>
-      <button onClick={this.increment} id={"-" + idNum}>Subtract {idNum}</button>
-    </li>
-    )
-  }
-
-  /**
-   * Render the current state of the odometer
-   */
   render() {
     return (
       <div className="App">
         <HeaderComponent />
-        <main>
-        <h2>{this.displayNumber(this.state.odometer)}</h2>
-          <ul>
-            {[0,1,2,3].map(val => this.createButtonSet(val))}
-          </ul>
-        </main>
+        <Odometer />
         <FooterComponent />
       </div>
-    );
+    )
   }
 }
 
